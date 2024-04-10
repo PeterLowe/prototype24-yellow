06c926467d72b44ece4bdb7380edbb2e7de6e6c5
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2024-04-10 10:42:16 +0100

    Added needed functions

--- Artefact Brawler/Artefact Brawler/MainMenu.cpp
+++ Artefact Brawler/Artefact Brawler/MainMenu.cpp
@@ -1,1 +1,13 @@
 #include "MainMenu.h"
+
+void MainMenu::processEvents(sf::Event t_event)
+{
+}
+
+void MainMenu::update(sf::Time t_deltaTime)
+{
+}
+
+void MainMenu::render(sf::RenderWindow& t_event)
+{
+}
