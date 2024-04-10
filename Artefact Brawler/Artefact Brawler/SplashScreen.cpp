45367627f173162018564b505ba0b494846019bc
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2024-04-10 10:37:47 +0100

    Added needed functions

--- Artefact Brawler/Artefact Brawler/SplashScreen.cpp
+++ Artefact Brawler/Artefact Brawler/SplashScreen.cpp
@@ -1,1 +1,17 @@
 #include "SplashScreen.h"
+
+
+void SplashScreen::processEvents(sf::Event t_event)
+{
+
+}
+
+void SplashScreen::update(sf::Time t_deltaTime)
+{
+
+}
+
+void SplashScreen::render(sf::RenderWindow& t_event)
+{
+
+}
