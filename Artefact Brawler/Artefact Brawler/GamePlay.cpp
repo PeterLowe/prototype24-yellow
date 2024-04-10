afaea75b5dec633e9da82656e22157974e2ff5bd
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2024-04-10 10:57:24 +0100

    Added needed functions to gameplay

--- Artefact Brawler/Artefact Brawler/GamePlay.cpp
+++ Artefact Brawler/Artefact Brawler/GamePlay.cpp
@@ -1,1 +1,13 @@
 #include "GamePlay.h"
+
+void GamePlay::processEvents(sf::Event t_event)
+{
+}
+
+void GamePlay::update(sf::Time t_deltaTime)
+{
+}
+
+void GamePlay::render(sf::RenderWindow& t_event)
+{
+}
--- Artefact Brawler/Artefact Brawler/GamePlay.h
+++ Artefact Brawler/Artefact Brawler/GamePlay.h
@@ -1,5 +1,16 @@
 #pragma once
+
+#include <SFML/Graphics.hpp>
+
 class GamePlay
 {
+public:
+
+	void processEvents(sf::Event t_event);
+	void update(sf::Time t_deltaTime);
+	void render(sf::RenderWindow& t_event);
+
+private:
+
 };
 
