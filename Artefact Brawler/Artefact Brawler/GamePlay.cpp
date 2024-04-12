00425971bbaa6e770d5b3a0b3eed3516adc68784
(Student  C00294680) Ian Perez Bunuel <C00294680@setu.ie>
2024-04-12 10:58:11 +0100

    Noy Working Currently (class ended)

--- Artefact Brawler/Artefact Brawler/GamePlay.cpp
+++ Artefact Brawler/Artefact Brawler/GamePlay.cpp
@@ -2,7 +2,29 @@
 
 void GamePlay::processEvents(sf::Event t_event)
 {
+	sf::Event newEvent;
+	while (m_window.pollEvent(newEvent))
+	{
+		if (sf::Event::KeyPressed == newEvent.type) //user pressed a key
+		{
+			processKeys(newEvent);
+		}
+	}
 }
+
+void GamePlay::processKeys(sf::Event t_event)
+{
+	// Player's Jump
+	if (sf::Keyboard::Space == t_event.key.code)
+	{
+		if (player.canJump)
+		{
+			player.jumping = true;
+			player.jumpAgain = true;
+		}
+	}
+}
+
 
 void GamePlay::update(sf::Time t_deltaTime)
 {
--- Artefact Brawler/Artefact Brawler/GamePlay.h
+++ Artefact Brawler/Artefact Brawler/GamePlay.h
@@ -1,16 +1,23 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include "Player.h"
 
 class GamePlay
 {
 public:
 
 	void processEvents(sf::Event t_event);
+	void processKeys(sf::Event t_event);
+
 	void update(sf::Time t_deltaTime);
 	void render(sf::RenderWindow& t_event);
 
 private:
 
+	void setupObjects();
+
+	// Objects
+	Player player;
 };
 
