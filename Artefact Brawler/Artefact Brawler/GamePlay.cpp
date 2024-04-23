b8823d7e649738b1f014869a76ca9f73435edf95
(Student  C00298242) Liam Treacy <C00298242@setu.ie>
2024-04-23 16:32:16 +0100

    bouncepad and merge errors fully fixed

--- Artefact Brawler/Artefact Brawler/GamePlay.cpp
+++ Artefact Brawler/Artefact Brawler/GamePlay.cpp
@@ -265,7 +265,7 @@ void GamePlay::update(sf::Time t_deltaTime)
 		/// Attacks ///
 		doAttacks();
 		doSpecials();
-
+		bouncePadCheck(bouncePad);
 		// Sandbags knockback
 		if (sandbag.knockingBack)
 		{
@@ -276,7 +276,7 @@ void GamePlay::update(sf::Time t_deltaTime)
 			knockbackAngle = sandbag.bounce(knockbackAngle);
 			sandbag.knockback(knockbackAngle, knockbackPower, damageTaken); // Knockback
 		}
-
+	
 		if (!canAttack)
 		{
 			// Do endlag timer
@@ -317,6 +317,7 @@ void GamePlay::render(sf::RenderWindow& t_window)
 		t_window.draw(platforms[i].getBody());
 	}
 
+	t_window.draw(bouncePad.getBouncePad());
 	// Sandbag's percentage
 	t_window.draw(sandbagPercentage);
 
@@ -547,31 +548,6 @@ void GamePlay::doAttacks()
 			}
 		}
 	}
-	bouncePadCheck(bouncePad);
-	// Sandbags knockback
-	if (sandbag.knockingBack)
-	{
-		// Change the percentage string
-		sandbagPercentage.setString("Sandbag: " + std::to_string(sandbag.percentage) + "%");
-
-		// Bouncing
-		knockbackAngle = sandbag.bounce(knockbackAngle);
-		sandbag.knockback(knockbackAngle, knockbackPower, damageTaken); // Knockback
-	}
-
-	if (!canAttack)
-	{
-		// Do endlag timer
-		endLag();
-
-		// Allow for another attack input from controller
-		buttonPressed = false;
-
-		// Allow damage again
-		damageDone = false;
-	}
-
-	player.changeColor(canAttack);
 }
 void GamePlay::drawAttacks(sf::RenderWindow& t_window)
 {
@@ -601,44 +577,7 @@ void GamePlay::drawAttacks(sf::RenderWindow& t_window)
 	}
 }
 
-
-
-void GamePlay::drawSpecials(sf::RenderWindow& t_window)
-{
-	// Platforms
-	for (int i = 0; i < 3; i++)
-	{
-		t_window.draw(platforms[i].getBody());
-	}
-
-	t_window.draw(bouncePad.getBouncePad());
-
-	// Sandbag's percentage
-	t_window.draw(sandbagPercentage);
-
-	// Coins Text
-	t_window.draw(coinsText);
-}
-
-void GamePlay::bouncePadCheck(ReflectiveBouncePads t_bouncingPad)
-{
-	if (sandbag.getBody().getGlobalBounds().intersects(t_bouncingPad.getBouncePad().getGlobalBounds()))
-	{
-		//knockbackAngle = AttackManager::getUpAngleD(); // Set angle
-		//knockbackPower = AttackManager::getUpPower(); // Set power
-
-		knockbackAngle = bouncePad.angle;
-		knockbackPower = bouncePad.power;
-
-		sandbag.hitAgain = true;
-		sandbag.onGround = true;
-		player.onGround = true;
-		sandbag.knockingBack = true;
-		bouncePad.hasHit = true;
-	}
-}
-
-void GamePlay::setupFontAndText()
+void GamePlay::doSpecials()
 {
 	// Neutral Attack
 	if (AttackManager::getNeutralSpecialActive()) // Attack
@@ -814,6 +753,7 @@ void GamePlay::setupFontAndText()
 		}
 	}
 }
+
 void GamePlay::drawSpecials(sf::RenderWindow& t_window)
 {
 	if (AttackManager::getNeutralSpecialActive())
@@ -839,11 +779,26 @@ void GamePlay::drawSpecials(sf::RenderWindow& t_window)
 	{
 		AttackManager::drawDownSpecial(t_window);
 	}
-	bouncePad.setUpBP({ (SCREEN_WIDTH / 3 + 5) * 3, (SCREEN_HEIGHT / 3)});
+	bouncePad.setUpBP({ (SCREEN_WIDTH / 3 + 5) * 3, (SCREEN_HEIGHT / 3) });
 
 	// Attack setups
 	AttackManager::setup();
 }
+
+void GamePlay::bouncePadCheck(ReflectiveBouncePads t_bouncingPad)
+{
+	if (sandbag.getBody().getGlobalBounds().intersects(t_bouncingPad.getBouncePad().getGlobalBounds()))
+	{
+		knockbackAngle = bouncePad.angle;
+		knockbackPower = bouncePad.power;
+
+		sandbag.hitAgain = true;
+		sandbag.onGround = true;
+		sandbag.knockingBack = true;
+		bouncePad.hasHit = true;
+	}
+}
+
 
 void GamePlay::endLag()
 {
