4cda437cf4ad259a878dcea4888dedeb649305bc
Soph Stanley <c00299461@setu.ie>
2024-04-23 22:53:05 +0100

    Added character select screen buttons

--- Artefact Brawler/Artefact Brawler/CharacterSelect.cpp
+++ Artefact Brawler/Artefact Brawler/CharacterSelect.cpp
@@ -4,21 +4,103 @@
 CharacterSelect::CharacterSelect()
 {
 	setupSprite();
+	setupButton();
 }
 
 void CharacterSelect::processEvents(sf::Event t_event)
 {
+	if (sf::Event::MouseButtonPressed == t_event.type)
+	{
+		processMouseDown(t_event);
+	}
+
+	if (sf::Event::MouseMoved == t_event.type)
+	{
+		processMouseMove(t_event);
+	}
+}
+
+void CharacterSelect::processMouseDown(sf::Event t_event)
+{
+	if (character1Colliding)
+	{
+		transitionCircle.transition(character1Button.sendTo);
+		std::cout << "Character 1" << std::endl;
+	}
+
+	if (character2Colliding)
+	{
+		transitionCircle.transition(character2Button.sendTo);
+		std::cout << "Character 2" << std::endl;
+	}
+
+	if (character3Colliding)
+	{
+		transitionCircle.transition(character3Button.sendTo);
+		std::cout << "Character 3" << std::endl;
+	}
+
+	if (character4Colliding)
+	{
+		transitionCircle.transition(character4Button.sendTo);
+		std::cout << "Character 4" << std::endl;
+	}
+
+	if (character5Colliding)
+	{
+		transitionCircle.transition(character5Button.sendTo);
+		std::cout << "Character 5" << std::endl;
+	}
+
+	if (character6Colliding)
+	{
+		transitionCircle.transition(character6Button.sendTo);
+		std::cout << "Character 6" << std::endl;
+	}
+}
+
+void CharacterSelect::processMouseMove(sf::Event t_event)
+{
+	mousePos.x = static_cast<float>(t_event.mouseMove.x);
+	mousePos.y = static_cast<float>(t_event.mouseMove.y);
 }
 
 void CharacterSelect::update(sf::Time t_deltaTime)
 {
+	character1Colliding = character1Button.checkForMouse(mousePos);
+	character2Colliding = character2Button.checkForMouse(mousePos);
+	character3Colliding = character3Button.checkForMouse(mousePos);
+	character4Colliding = character4Button.checkForMouse(mousePos);
+	character5Colliding = character5Button.checkForMouse(mousePos);
+	character6Colliding = character6Button.checkForMouse(mousePos);
+
+	if (transitionCircle.active)
+	{
+		transitionCircle.transition(transitionCircle.goTo);
+	}
 }
 
 void CharacterSelect::render(sf::RenderWindow& t_window)
 {
-	t_window.draw(character2Sprite);
+	t_window.draw(character1Button.getBody());
+	t_window.draw(character2Button.getBody());
+	t_window.draw(character3Button.getBody());
+	t_window.draw(character4Button.getBody());
+	t_window.draw(character5Button.getBody());
+	t_window.draw(character6Button.getBody());
+
+	// character sprites
 	t_window.draw(character1Sprite);
+	t_window.draw(character2Sprite);
 	t_window.draw(character3sprite);
+
+	t_window.draw(frameSprite); // frame/border/background for character select screen
+
+	// screen transition
+	if (transitionCircle.active)
+	{
+		t_window.draw(transitionCircle.getBody());
+	}
 }
 
 
@@ -49,4 +131,26 @@ void CharacterSelect::setupSprite()
 	}
 	character3sprite.setTexture(character3Texture);
 	character3sprite.setPosition(300.0f, 200.0f);
+
+	if (!frameTexture.loadFromFile("ASSETS\\IMAGES\\CharacterSelectFrame.png"))
+	{
+		std::cout << "Error loading frame" << std::endl;
+	}
+	frameSprite.setTexture(frameTexture);
+	frameSprite.setScale(2.0f, 2.0f);
+}
+
+void CharacterSelect::setupButton()
+{
+	character1Button.setup(character1Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Red);
+
+	character2Button.setup(character2Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Blue);
+
+	character3Button.setup(character3Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Green);
+
+	character4Button.setup(character4Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Yellow);
+
+	character5Button.setup(character5Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Magenta);
+
+	character6Button.setup(character6Pos, buttonWidth, buttonHeight, Scene::GamePlay, sf::Color::Cyan);
 }
--- Artefact Brawler/Artefact Brawler/CharacterSelect.h
+++ Artefact Brawler/Artefact Brawler/CharacterSelect.h
@@ -2,6 +2,8 @@
 
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include "Button.h"
+#include "ScreenTransition.h"
 
 class CharacterSelect // LIAM
 {
@@ -9,14 +11,19 @@ public:
 
 	CharacterSelect();
 	void processEvents(sf::Event t_event);
+	void processMouseDown(sf::Event t_event);
+	void processMouseMove(sf::Event t_event);
 	void update(sf::Time t_deltaTime);
 	void render(sf::RenderWindow& t_window);
 
 	void setupSprite();
+	void setupButton();
 
 	sf::Sprite getChar1Body();
 
 private:
+
+	sf::Vector2f mousePos = {};
 
 	sf::Sprite character1Sprite;
 	sf::Texture character1Texture;
@@ -24,5 +31,37 @@ private:
 	sf::Texture character2Texture;
 	sf::Sprite character3sprite;
 	sf::Texture character3Texture;
+
+	sf::Texture frameTexture;
+	sf::Sprite frameSprite;
+
+	Button character1Button;
+	sf::Vector2f character1Pos = { 329.0f, 405.0f };
+	bool character1Colliding = false;
+
+	Button character2Button;
+	sf::Vector2f character2Pos = { 544.0f, 405.0f };
+	bool character2Colliding = false;
+
+	Button character3Button;
+	sf::Vector2f character3Pos = { 758.0f, 405.0f };
+	bool character3Colliding = false;
+
+	Button character4Button;
+	sf::Vector2f character4Pos = { 329, 585.0f };
+	bool character4Colliding = false;
+
+	Button character5Button;
+	sf::Vector2f character5Pos = { 544.0f, 585.0f };
+	bool character5Colliding = false;
+
+	Button character6Button;
+	sf::Vector2f character6Pos = { 758.0f, 585.0f };
+	bool character6Colliding = false;
+
+	int buttonHeight = 170;
+	int buttonWidth = 200;
+
+	ScreenTransition transitionCircle;
 };
 
